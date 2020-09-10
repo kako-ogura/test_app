1ed6dcb9daebf63ae73164dca68a2527910d191d
kako_ogura <your.email@example.com>
2020-09-10 12:26:25 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #ページがもし正しく表示されなかった時のために返すヘルパー
+  def full_title(page_title = '')
+    base_title = "Rails"
+    if page_title.empty?
+      base_title
+    else
+      page_title + "|" + base_title
+    end
+  end
 end
