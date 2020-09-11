02ec2231031e5e4f5c5695fd2ee9f6ebf119961e
kako_ogura <your.email@example.com>
2020-09-11 14:13:57 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
-  def new
+
+  #ユーザーの一覧が見ることができる
+  def show
+    @user = User.find(params[:id])
   end
+
+  #signupする
+  def new
+    @user = User.new
+  end
+
+  #保存する
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Test App!"
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  #キーワード以降のコードを強調させるため
+  private
+
+   def user_params
+     #:user属性を必須とし、名前、メールアドレス、パスワード、パスワードの確認の属性をそれぞれ許可し、それ以外を許可しないようにする
+     params.require(:user).permit(:firstname,:lastname,:email,:password,:password_confirmation)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get '/help', to:'static_pages#help' #as:'helf'
   get '/about', to:'static_pages#about'
   get '/contact', to:'static_pages#contact'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # URL(/users/1)を追加する
+  resources :users
 end
