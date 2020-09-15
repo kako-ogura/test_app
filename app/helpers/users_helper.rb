35df0d8b265f5b841b662c61e6787090e00c5667
kako_ogura <your.email@example.com>
2020-09-15 12:49:58 +0900

    create index and show pages

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,3 @@
 module UsersHelper
 
-  def gravatar_for(user)
-    gravatar_id = Digest::MD5::hexdigest(user.firstname.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.firstname, class: "gravatar")
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get '/signup', to:'users#new'
 
+  get '/index', to:'users#index'
+
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
