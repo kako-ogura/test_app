f10a9316d6115c6bc5cacfae98b82ec75731cd5e
kako_ogura <your.email@example.com>
2020-09-16 18:20:16 +0900

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,20 @@
+
 Rails.application.routes.draw do
-  get 'password_resets/new'
-  get 'password_resets/edit'
-  root 'static_pages#home'
-  # as: でルートの置き換えができる
-  get '/help', to:'static_pages#help' #as:'helf'
-  get '/about', to:'static_pages#about'
-  get '/contact', to:'static_pages#contact'
-
-  get '/signup', to:'users#new'
-
-  get '/index', to:'users#index'
-
-  get '/account_activations/:id/edit', to:'account_activations#edit'
-
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-  # URL(/users/1)を追加する
-  resources :users
+  root                'static_pages#home'
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
-  resources :password_resets, only: [:new, :create, :edit, :update]
-  resources :microposts, only: [:create, :destroy]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
