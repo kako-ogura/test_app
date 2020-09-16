9c2563be64334e7e08ac6956ba8adc8619625c72
kako_ogura <your.email@example.com>
2020-09-16 11:56:58 +0900

    Add resource for account activation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_080405) do
+ActiveRecord::Schema.define(version: 2020_09_16_014758) do
 
   create_table "users", force: :cascade do |t|
     t.string "firstname"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_09_15_080405) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.boolean "admin", default: false
+    t.string "remember_digest"
   end
 
 end
